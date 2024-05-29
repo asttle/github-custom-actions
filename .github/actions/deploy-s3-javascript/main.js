e00d35886a0742b41db4f3f0b768580216361651
asttle-vuram <asttlej@workhall.com>
2024-05-29 17:38:19 +0100

    custom javascript actions

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec'); 
+const github = require('@actions/gtihub'); 
+
+
+function run() {
+    core.notice('Hello from my custom ')
+}
+
+run();
